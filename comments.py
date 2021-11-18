ed1afa9b1165e9c14097034802bbe1a8b07cda11
Xiaoyang Liu <xiaoyang.liu@stonybrook.edu>
2021-11-18 18:51:28 -0500

    add2

--- comments.py
+++ comments.py
@@ -11,3 +11,10 @@ sum_len=0
 for d in data:
 	sum_len += len(d)
 print('average', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('total <100', len(new))
+print(new[0])
