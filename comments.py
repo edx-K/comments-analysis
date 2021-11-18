2fc2242b85ae32a8289845f0224cd96f473f2d71
Xiaoyang Liu <xiaoyang.liu@stonybrook.edu>
2021-11-18 18:36:12 -0500

    add

--- comments.py
+++ comments.py
@@ -1,8 +1,8 @@
 data = []
+count=0
 with open ('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-print(len(data))
-print(data[0])
-print('---------')
-print(data[1])
+		count += 1
+		if count % 1000 ==0:
+			print(len(data))
