f40f3d62a7c2d55c6ca1bf3c85ea999bd55b2120
Xiaoyang Liu <xiaoyang.liu@stonybrook.edu>
2021-11-19 18:56:12 -0500

    2

--- saving.py
+++ saving.py
@@ -4,10 +4,17 @@ while True:
 	if name =='q':
 		break
 	price = input('price:')
+	price = int(price)
 	#p=[]
 	#p.append(name)
 	#p.append(price)
-	p = [name,price]
-	products.append(p)
+	#p = [name,price]
+	products.append([name,price])
 print(products)
 print(products[0][0])
+for p in products:
+	print(p[0],'price is :',p[1])
+with open('products.csv','w',encoding = 'utf-8') as f:
+	f.write('product,price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
