2c57ae9faca39643cbc3d9dabc1b20a0a04f1e1e
Xiaoyang Liu <xiaoyang.liu@stonybrook.edu>
2021-11-19 20:03:16 -0500

    4

--- readout.py
+++ readout.py
@@ -1,12 +1,16 @@
-#readout
+import os 
 product=[]
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if 'product,price' in line:
-			continue
-		name, price = line.strip().split(',')
-		product.append([name, price])
-print(product)
+if os.path.isfile('products.csv'):
+	print('Yes')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if 'product,price' in line:
+				continue
+			name, price = line.strip().split(',')
+			product.append([name, price])
+	print(product)
+else:
+	print('No')
 #input
 while True:
 	name = input('product:')
@@ -18,15 +22,15 @@ while True:
 	#p.append(name)
 	#p.append(price)
 	#p = [name,price]
-	products.append([name,price])
-print(products)
+	product.append([name,price])
+print(product)
 #record
-for p in products:
+for p in product:
 	print(p[0],'price is :',p[1])
 #write in
 with open('products.csv','w', encoding = 'utf-8') as f:
 	f.write('product,price\n')
-	for p in products:
+	for p in product:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
 
 
