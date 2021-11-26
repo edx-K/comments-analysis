273a22baec70c9c8089eeba1c8a57a5be9ff0fc0
Xiaoyang Liu <xiaoyang.liu@stonybrook.edu>
2021-11-26 17:56:56 -0500

    reactor

---
+++ products_price.py
@@ -0,0 +1,43 @@
+import os
+#read in original file
+def read_file(filename):
+	products=[]
+	if os.path.isfile(filename):#check if it's there
+		print('find it')
+		with open (filename,'r',encoding = 'utf-8') as f:
+			for line in f:
+				 if 'products,price' in line:
+				 	continue
+				 name, price =line.strip().split(',')
+				 products.append([name, price])
+		print(products)
+	else:
+		print('cant find file')
+	return products
+#user input
+def user_input(products):
+	while True:
+		name = input('please put in product name:')
+		if name =='q':
+			break
+		price = input('please input product price:')
+		price = int(price)
+		products.append([name,price])
+	print(products)
+	return products
+#print all the product record
+def print_products(products):
+	for p in products:
+		print(p[0], 'price is :', p[1])
+#write in file:
+def write_in(filename, products):
+	with open(filename,'w',encoding = 'utf-8')as f:
+		f.write('product,price\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+write_in('products.csv', products)
