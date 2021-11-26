71102ef2189a59eeb1d0f1d55eb9eedc24852f65
Xiaoyang Liu <xiaoyang.liu@stonybrook.edu>
2021-11-26 18:48:21 -0500

    correct repeat function refactor

--- products_price.py
+++ products_price.py
@@ -2,18 +2,14 @@ import os
 #read in original file
 def read_file(filename):
 	products=[]
-	if os.path.isfile(filename):#check if it's there
-		print('find it')
-		with open (filename,'r',encoding = 'utf-8') as f:
-			for line in f:
-				 if 'products,price' in line:
-				 	continue
-				 name, price =line.strip().split(',')
-				 products.append([name, price])
-		print(products)
-	else:
-		print('cant find file')
+	with open (filename,'r',encoding = 'utf-8') as f:
+		for line in f:
+			if 'products,price' in line:
+				continue
+			name, price =line.strip().split(',')
+			products.append([name, price])
 	return products
+
 #user input
 def user_input(products):
 	while True:
@@ -25,10 +21,12 @@ def user_input(products):
 		products.append([name,price])
 	print(products)
 	return products
+
 #print all the product record
 def print_products(products):
 	for p in products:
 		print(p[0], 'price is :', p[1])
+
 #write in file:
 def write_in(filename, products):
 	with open(filename,'w',encoding = 'utf-8')as f:
@@ -37,7 +35,16 @@ def write_in(filename, products):
 			f.write(p[0] + ',' + str(p[1]) + '\n')
 
 
-products = read_file('products.csv')
-products = user_input(products)
-print_products(products)
-write_in('products.csv', products)
+#main function
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):#check if it's there
+		print('find it')
+		products = read_file(filename)
+	else:
+		print('cant find file')
+	products = user_input(products)
+	print_products(products)
+	write_in(filename, products)
+
+main()
